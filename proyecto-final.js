6723760f3186e127a0c5a1f654afe0bbbb59547a
maky <makysolmartinezv@gmail.com>
2022-12-13 17:54:56 -0300

    subiendo mi primer commit

---
+++ proyecto-final.js
@@ -0,0 +1,22 @@
+var edad =prompt("Cual es tu edad?");
+
+if(edad >= 18) {
+    console.log("Puede ingresar");
+}else {
+    alert("No puede ingresar ya que es menor");
+}
+.carta{
+    border-style: solid;
+    border-color: black;
+    border-radius: 4px;
+    margin: 150px;
+   
+    
+}
+.carta-presentacion{
+    float: left;
+    width: 15%;
+}
+.texto-carta-presentacion{
+   padding-right: 0px;
+}
